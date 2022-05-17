09f2b7d25c8363f884edd9d3741246b02290885b
Im-Eager <83980495+Im-Eager@users.noreply.github.com>
2022-05-17 09:58:51 +0100

    added loggin form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,3 +1,11 @@
-function LoginForm(){
-    return <div>wecme</div>
+function LoginForm() {
+    return (
+        <div>
+            <button>X</button>
+            <img src="" />
+            <main>Bem-vindo(a) ao Pinterest</main>
+            <form></form>
+        </div>
+    );
 }
+export { LoginForm };
