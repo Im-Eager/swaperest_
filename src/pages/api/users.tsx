74969fe399fa66b76f483c2f584c6c0c426850d2
Joao Souto <jpsoutost@gmail.com>
2022-05-17 09:46:53 +0100

    added apis and started to create component login form

---
+++ src/pages/api/users.tsx
@@ -0,0 +1,20 @@
+import { connectToDatabase } from "../../../util/mongodb";
+
+export default async function handler(req, res) {
+  
+    const { db } = await connectToDatabase();
+    
+    if (req.method === "POST") {
+    const gamesCollection = db.collection("users");
+    const result = await gamesCollection.insertOne(req.body);
+    res.status(201).json({ message: "Data inserted successfully!" });
+  }
+
+  if (req.method === "GET"){
+    const editionsCollection = db.collection("users")
+    const findResult = await editionsCollection.find({}).toArray();
+    return res.json(findResult);
+    
+  }
+
+}
